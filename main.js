d9c20be1e837e9039d046649dc2f869e374e3b49
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-02-24 19:53:30 +0900

    show datas

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const prisma = new PrismaClient()
 
 async function main() {
     console.log('start main')
+    const users = await prisma.user.findMany()
+    console.log(users);
 }
 
 main()
@@ -13,3 +15,14 @@ main()
   .finally(async () => {
     await prisma.$disconnect()
   })
+
+//   import { PrismaClient } from '@prisma/client'
+
+// // Fetch all posts (in /pages/index.tsx)
+// export async function getStaticProps() {
+//   const prisma = new PrismaClient()
+//   const posts = await prisma.post.findMany()
+//   return {
+//     props : { posts }
+//   }
+// }
